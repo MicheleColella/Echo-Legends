a92b0352a820c41c5fdf3902d6a1a772f18c943e
Michele Colella <michelecolella0@gmail.com>
2024-07-02 23:06:57 +0200

    Aggiornato il sistema di input con quello nuovo

--- Assets/KinematicCharacterController/ExampleCharacter/Scripts/ExamplePlayer.cs
+++ Assets/KinematicCharacterController/ExampleCharacter/Scripts/ExamplePlayer.cs
@@ -3,33 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using KinematicCharacterController;
 using KinematicCharacterController.Examples;
+using UnityEngine.InputSystem;
 
 namespace KinematicCharacterController.Examples
 {
     public class ExamplePlayer : MonoBehaviour
     {
         public ExampleCharacterController Character;
-        //public ExampleCharacterCamera CharacterCamera;
 
-        private const string MouseXInput = "Mouse X";
-        private const string MouseYInput = "Mouse Y";
-        private const string MouseScrollInput = "Mouse ScrollWheel";
-        private const string HorizontalInput = "Horizontal";
-        private const string VerticalInput = "Vertical";
+        private InputActions playerInputActions;
+        private Vector2 moveInput;
+
+        private void Awake()
+        {
+            playerInputActions = new InputActions();
+        }
+
+        private void OnEnable()
+        {
+            playerInputActions.Player.Enable();
+            playerInputActions.Player.Move.performed += OnMove;
+            playerInputActions.Player.Move.canceled += OnMove;
+        }
+
+        private void OnDisable()
+        {
+            playerInputActions.Player.Disable();
+            playerInputActions.Player.Move.performed -= OnMove;
+            playerInputActions.Player.Move.canceled -= OnMove;
+        }
 
         private void Start()
         {
             Cursor.lockState = CursorLockMode.Locked;
-
-            //CharacterCamera.SetFollowTransform(Character.CameraFollowPoint);
-
-            //CharacterCamera.IgnoredColliders.Clear();
-            //CharacterCamera.IgnoredColliders.AddRange(Character.GetComponentsInChildren<Collider>());
         }
 
         private void Update()
         {
-            if (Input.GetMouseButtonDown(0))
+            if (Mouse.current.leftButton.wasPressedThisFrame)
             {
                 Cursor.lockState = CursorLockMode.Locked;
             }
@@ -37,63 +48,19 @@ namespace KinematicCharacterController.Examples
             HandleCharacterInput();
         }
 
-        private void LateUpdate()
-        {
-            /*
-            // Handle rotating the camera along with physics movers
-            if (CharacterCamera.RotateWithPhysicsMover && Character.Motor.AttachedRigidbody != null)
-            {
-                CharacterCamera.PlanarDirection = Character.Motor.AttachedRigidbody.GetComponent<PhysicsMover>().RotationDeltaFromInterpolation * CharacterCamera.PlanarDirection;
-                CharacterCamera.PlanarDirection = Vector3.ProjectOnPlane(CharacterCamera.PlanarDirection, Character.Motor.CharacterUp).normalized;
-            }
-            */
-
-            HandleCameraInput();
-        }
-
-        private void HandleCameraInput()
-        {
-            // Create the look input vector for the camera
-            float mouseLookAxisUp = Input.GetAxisRaw(MouseYInput);
-            float mouseLookAxisRight = Input.GetAxisRaw(MouseXInput);
-            Vector3 lookInputVector = new Vector3(mouseLookAxisRight, mouseLookAxisUp, 0f);
-
-            // Prevent moving the camera while the cursor isn't locked
-            if (Cursor.lockState != CursorLockMode.Locked)
-            {
-                lookInputVector = Vector3.zero;
-            }
-
-            // Input for zooming the camera (disabled in WebGL because it can cause problems)
-            float scrollInput = -Input.GetAxis(MouseScrollInput);
-#if UNITY_WEBGL
-        scrollInput = 0f;
-#endif
-
-            // Apply inputs to the camera
-            //CharacterCamera.UpdateWithInput(Time.deltaTime, scrollInput, lookInputVector);
-
-            // Handle toggling zoom level
-            if (Input.GetMouseButtonDown(1))
-            {
-                //CharacterCamera.TargetDistance = (CharacterCamera.TargetDistance == 0f) ? CharacterCamera.DefaultDistance : 0f;
-            }
-        }
-
         private void HandleCharacterInput()
         {
             PlayerCharacterInputs characterInputs = new PlayerCharacterInputs();
 
-            // Build the CharacterInputs struct
-            characterInputs.MoveAxisForward = Input.GetAxisRaw(VerticalInput);
-            characterInputs.MoveAxisRight = Input.GetAxisRaw(HorizontalInput);
-            //characterInputs.CameraRotation = CharacterCamera.Transform.rotation;
-            characterInputs.JumpDown = Input.GetKeyDown(KeyCode.Space);
-            characterInputs.CrouchDown = Input.GetKeyDown(KeyCode.C);
-            characterInputs.CrouchUp = Input.GetKeyUp(KeyCode.C);
+            characterInputs.MoveAxisForward = moveInput.y;
+            characterInputs.MoveAxisRight = moveInput.x;
 
-            // Apply inputs to character
             Character.SetInputs(ref characterInputs);
+        }
+
+        private void OnMove(InputAction.CallbackContext context)
+        {
+            moveInput = context.ReadValue<Vector2>();
         }
     }
 }
